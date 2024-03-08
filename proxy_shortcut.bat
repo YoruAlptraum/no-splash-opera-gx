83cba758219ce08c026dcf153a2c565942746149
Thiago Ozawa <72313676+YoruAlptraum@users.noreply.github.com>
2024-03-08 17:49:57 -0300

    copy trigger and proxy shortcut scripts added
    added the initial scripts to copy task trigger and to create a proxy shortcut to open opera
    wip

---
+++ proxy_shortcut.bat
@@ -0,0 +1,8 @@
+@REM proxy opera shortcut to run the script when opening opera
+
+@REM taskbar path %userprofile%\appdata\Roaming\Microsoft\Internet Explorer\Quick Launch\User Pinned\TaskBar
+
+@REM go to taskbar icon
+@REM get exe path
+@REM create script that executes in sequence the opera launcher and the no splash exe
+
