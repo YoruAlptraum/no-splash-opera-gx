982a19ef34570240027be34d133a8ed6f1010ce9
Thiago Daisuke Ozawa <yorualptraum@gmail.com>
2024-03-10 16:49:31 -0300

    Cleaned up previous versions files
    deleted files used on previous versions that are no longer necessary

--- proxy_shortcut.bat
+++
@@ -1,8 +0,0 @@
-@REM proxy opera shortcut to run the script when opening opera
-
-@REM taskbar path %userprofile%\appdata\Roaming\Microsoft\Internet Explorer\Quick Launch\User Pinned\TaskBar
-
-@REM go to taskbar icon
-@REM get exe path
-@REM create script that executes in sequence the opera launcher and the no splash exe
-
